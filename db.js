b6cd09fb0b405d624e8526848e7837d3c87f5983
joseadolfo10 <139913282+joseadolfo10@users.noreply.github.com>
2024-03-26 00:08:17 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,19 +1,20 @@
-const mysql = require('mysql’); // LEMBRAR: $ npm install mysql
+const mysql = require('mysql2'); // LEMBRAR: $ npm install mysql
 
-const app = express();
 // Configuração da conexão com o banco de dados
 const connection = mysql.createConnection({
-host: 'localhost',
-user: 'seu_usuario',
-password: 'sua_senha',
-database: 'nomedoBancoDeDados'
-});
+    host: process.env.host, // Endereço do servidor MySQL
+    port: process.env.port , // Porta do servidor MySQL
+    user: process.env.user, // Nome de usuário do MySQL
+    password: process.env.password, // Senha do usuário do MySQL
+    database: process.env.database // Nome do banco de dados que deseja se conectar
+  });
+
 // Conexão com o banco de dados
 connection.connect((err) => {
-if (err) {
-console.error('Erro ao conectar ao banco de dados:', err);
-return;
-}
-console.log('Conexão bem-sucedida ao banco de dados MySQL');
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão bem-sucedida ao banco de dados MySQL');
 });
 module.exports = connection;
